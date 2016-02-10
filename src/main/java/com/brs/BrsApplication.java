177065113a0bb8f8e3433b8c6074432e929b1ef1
unknown <Shruti.c@ShrutiC-PC.abhsindia.local>
2016-02-10 10:24:55 +0530

    Created the basic project structure

---
+++ src/main/java/com/brs/BrsApplication.java
@@ -0,0 +1,12 @@
+package com.brs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrsApplication.class, args);
+	}
+}
